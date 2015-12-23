3c790aa461c7049a06b782cdfd7a37b79cd4730a
crazle <faintday@outlook.com>
2015-12-23 18:49:46 +0800

    Modify the Course Entity Create the Department Entity Modify the Enrollment Entity

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,4 +1,6 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
 {
     public enum Grade { A, B, C, D, F }
 
@@ -7,6 +9,7 @@
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No Grade")]
         public Grade? Grade { get; set; }
         public virtual Course Course { get; set; }
         public virtual Student Student { get; set; }
