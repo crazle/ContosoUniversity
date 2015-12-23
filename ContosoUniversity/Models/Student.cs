59f969358729ca89fae1899c1706eced81010e4f
crazle <faintday@outlook.com>
2015-12-23 18:08:01 +0800

    加入DataType和DisplayFormat. The ApplyFormatInEditMode setting specifies that the specified formatting should also be applied when the value is displayed in a text box for editing

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace ContosoUniversity.Models
 {
     public class Student
@@ -11,7 +11,10 @@ namespace ContosoUniversity.Models
         public int Id { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
